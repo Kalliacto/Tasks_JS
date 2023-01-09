77dba8e885ca382f7d2380411bc4601756699409
Kalliacto <98468178+Kalliacto@users.noreply.github.com>
2023-01-09 12:19:43 +0300

    Сравнение строк

---
+++ 1.7_unicode_numeric_values.js
@@ -0,0 +1,10 @@
+let one = (symbols.codePointAt(0));
+let two = (symbols.codePointAt(1));
+let three = (fc1+fc2);
+
+if ((three % 2) == 0){
+	console.log(String.fromCodePoint(three));
+}
+else {
+	console.log("Символ обнаружить не удалось");
+}
