cadc14ad5a3935f8d74e993c4646f51826ed7747
Kalliacto <98468178+Kalliacto@users.noreply.github.com>
2023-01-12 23:25:07 +0300

    вариант через метод filter

--- 2.2_create_a_new_array_based_on_the_variable.js
+++ 2.2_create_a_new_array_based_on_the_variable.js
@@ -10,3 +10,9 @@ function newArray(value) {
 	console.log(newArr);
 }
 newArray(partNumbers);
+
+//Или вариант через метод filter
+ 
+let regexp= /\d\d\D\D$/;
+const resultArr2 = partNumbers.filter(item => regexp.test(item));
+console.log(resultArr2);
