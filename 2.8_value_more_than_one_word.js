df13a7a6f0290adc329d3f9ce12b6b64764a3395
Kalliacto <98468178+Kalliacto@users.noreply.github.com>
2023-01-16 14:30:50 +0300

    Методы для поиска элементов в массиве/indexOf

--- 2.8_value_more_than_one_word.js
+++ 2.8_value_more_than_one_word.js
@@ -4,6 +4,7 @@ let users = [
     {login: "user3", role: "Moderator"},
     {login: "user2", role: "State user"},
 ];
+//Поиск количества элементов содержащих более 1 слова
 
 function findfMoreThanOneWord (vulue) {
     let countResult = 0;
@@ -14,5 +15,17 @@ function findfMoreThanOneWord (vulue) {
     }
         
     console.log(countResult);
+};
+findfMoreThanOneWord (users);
+
+
+//Поиск индекса элемента содержего более 1 слова
+
+function findfMoreThanOneWord (vulue) {
+    for(let i = 0; i < vulue.length; i++) {
+        if (((vulue[i].role).split(' ')).length > 1) {
+            console.log(vulue.indexOf(vulue[i]));
+        }
+    }
 };
 findfMoreThanOneWord (users);
