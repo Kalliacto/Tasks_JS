dc501aa7aac7ef9910a275993c624d79e537f47e
Kalliacto <98468178+Kalliacto@users.noreply.github.com>
2023-01-23 20:22:59 +0300

    Update 2.9_sort_array.js

--- 2.9_sort_array.js
+++ 2.9_sort_array.js
@@ -11,3 +11,14 @@ const sortArray = (arr) => {
 };
 
 sortArray(array);
+
+// ----------------------------------
+
+
+const compare = (a, b) => {
+  if (a.length == b.length) return 0;
+  if (a.length < b.length) return -1;
+  if (a.length > b.length) return 1;
+}
+
+array.sort(compare);
