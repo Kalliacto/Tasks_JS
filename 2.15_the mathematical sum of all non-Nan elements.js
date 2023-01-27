5a05314e80db79b17a9a611684f251ac9f3f273d
Kalliacto <98468178+Kalliacto@users.noreply.github.com>
2023-01-27 20:49:56 +0300

    Методы reduce и reduceRight

---
+++ 2.15_the mathematical sum of all non-Nan elements.js
@@ -0,0 +1,30 @@
+let values = ["100", "30", "Не число", "20", "Тоже не число"];
+
+
+const calcSumElements = (array) => {
+let resultSum = 0;
+
+for (i = 0;i < array.length; i++ ) {
+	if (isNaN(array[i]) !== true) {
+	resultSum += +array[i];
+}
+}
+return resultSum;
+}
+
+calcSumElements(values);
+
+
+// -------------------------------------------------------------
+
+let values = ["100", "30", "Не число", "20", "Тоже не число"];
+
+
+const calcSumElements = values.reduce(function(previousValue, currentItem) {
+            if (isNaN(currentItem) !== true) {
+        	return +previousValue + +currentItem;
+            } else {
+                return +previousValue;
+            }   
+}, 0);
+calcSumElements;
