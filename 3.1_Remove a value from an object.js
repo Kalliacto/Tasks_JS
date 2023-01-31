63f0e84ee016ecd644a19171e5d0a2408407587e
Kalliacto <98468178+Kalliacto@users.noreply.github.com>
2023-01-31 18:29:52 +0300

    Удалить значение из объекта

---
+++ 3.1_Remove a value from an object.js
@@ -0,0 +1,19 @@
+Пример значений переменных:
+
+car  = {
+    model: "Audi", 
+    color: "white",
+    weight: 1850,
+    engine: "170 hp"
+}
+Пример результата:
+
+{
+    model: "Audi", 
+    color: "white",
+    weight: 1850
+}
+
+
+delete car.engine;
+console.log(car);
