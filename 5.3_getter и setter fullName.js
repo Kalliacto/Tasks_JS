ed5106950b42b224b2a0c1b424b9708c88f13910
Kalliacto <98468178+Kalliacto@users.noreply.github.com>
2023-02-07 18:29:13 +0300

    getter и setter fullName

---
+++ 5.3_getter и setter fullName.js
@@ -0,0 +1,27 @@
+В программе объявлены переменные firstName и lastName. Создайте объект user со свойствами firstName и lastName и добавьте getter и setter fullName. Сеттер fullName должен принимать имя и фамилию пользователя через пробел, разделять и присваивать свойствам firstName и lastName соответственно.
+Геттер fullName должен формировать строку, состоящую из имени и фамилии через пробел.
+
+Пример значений переменных:
+
+firstName = "Александр"
+lastName = "Петров"
+Пример результата:
+
+user.fullName // "Александр Петров"
+user.fullName = "Всеволод Старозубов"
+user.firstName // "Всеволод"
+user.lastName // "Старозубов"
+
+//----------------------------------------------
+let user = {
+    firstName,
+    lastName,
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+    
+  set fullName(value) {
+    return this.array = value;
+    } 
+}
