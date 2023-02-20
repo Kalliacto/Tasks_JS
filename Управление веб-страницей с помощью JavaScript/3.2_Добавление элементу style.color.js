62840b41e1330d5d21f5ed312a556a0fcc64caaf
Kalliacto <98468178+Kalliacto@users.noreply.github.com>
2023-02-20 13:31:19 +0300

    Работа с классами в DOM.

---
+++ Управление веб-страницей с помощью JavaScript/3.2_Добавление элементу style.color.js
@@ -0,0 +1,5 @@
+В HTML-документе задан тег <p> с идентификатором rainbow. Напишите скрипт, который указывает в стиле синий цвет текста (#2980b9).
+
+const element = document.getElementById('rainbow');
+element.style.color = "#2980b9";
+
